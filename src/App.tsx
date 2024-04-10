1d418a759ced69179a82fe0766b7144d399ae4ee
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-04-10 21:58:27 +0300

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
   return (
-    <div>App</div>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      gap="1"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem pl="2" bg="orange.300" area={"nav"}>
+        nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl="2" bg="pink.300" area={"aside"}>
+          aside
+        </GridItem>
+      </Show>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
