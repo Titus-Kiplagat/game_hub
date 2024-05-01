4007b8f4b39980fb5936747c40e54dd281819ff3
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-05-01 20:33:58 +0300

    Improve game detail page layout

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,26 +1,29 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttribute";
-import GameTrailer from "../components/GameTrailer";
 import GameScreenshots from "../components/GameScreenshots";
+import GameTrailer from "../components/GameTrailer";
+import useGame from "../hooks/useGame";
 
 const GameDetailsPage = () => {
-
   const { slug } = useParams();
-	const { data: game, isPending, error } = useGame(slug!);
+  const { data: game, isPending, error } = useGame(slug!);
 
-	if (isPending) return <Spinner />;
-	if (error && !game) throw error;
+  if (isPending) return <Spinner />;
+  if (error && !game) throw error;
   return (
-    <>
-      <Heading>{game.name}</Heading>
-			<ExpandableText>{game.description_raw}</ExpandableText>
-			<GameAttributes game={game} />
-			<GameTrailer gameId={game.id} />
-			<GameScreenshots gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <GameScreenshots gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
