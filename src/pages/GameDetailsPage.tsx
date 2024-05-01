1fafede3e5f2993cd3f3baec98a93c45022c04ff
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-05-01 18:57:25 +0300

    Display game attributes

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,16 +1,21 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttribute";
 
 const GameDetailsPage = () => {
 
   const { slug } = useParams();
-  const { data: game } = useGame(slug!);
+	const { data: game, isPending, error } = useGame(slug!);
+
+	if (isPending) return <Spinner />;
+	if (error && !game) throw error;
   return (
     <>
-      <Heading>{game?.name}</Heading>
-			<ExpandableText>{game?.description_raw}</ExpandableText>
+      <Heading>{game.name}</Heading>
+			<ExpandableText>{game.description_raw}</ExpandableText>
+			<GameAttributes game={game} />
     </>
   );
 };
