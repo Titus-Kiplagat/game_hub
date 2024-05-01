2c51fb6a3d8a8308f46d558669b978676fc44a2b
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-05-01 18:13:51 +0300

    Add show more or less description functionality

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,17 +1,18 @@
+import { Heading } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame"
-import { Heading } from '@chakra-ui/react';
-import { Text } from '@chakra-ui/react';
+import useGame from "../hooks/useGame";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailsPage = () => {
-	const { slug } = useParams();
-	const { data: game } = useGame(slug!);
-	return (
-		<>
-			<Heading>{game?.name}</Heading>
-			<Text>{game?.description_raw}</Text>
-		</>
-	)
-}
 
-export default GameDetailsPage
+  const { slug } = useParams();
+  const { data: game } = useGame(slug!);
+  return (
+    <>
+      <Heading>{game?.name}</Heading>
+			<ExpandableText>{game?.description_raw}</ExpandableText>
+    </>
+  );
+};
+
+export default GameDetailsPage;
